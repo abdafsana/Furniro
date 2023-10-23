c85017398ca85aa19f8ebb6cdabea4e2a04c349d
abdafsana <abdullayevaynur527@gmail.com>
2023-10-23 17:09:44 +0400

    four commit

---
+++ assets/js/search.js
@@ -0,0 +1,5 @@
+let icon=document.querySelectorAll('.navbar-item-icons svg')[0];
+let search= document.querySelector('.modal-navbar-item');
+// icon.addEventListener("click",function(){
+//     search.style.display="block";
+// })
